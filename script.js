dcf40ef9af095787bd8934f720c2f6359ff871eb
Carolina Pavan <116219003+PavanCarol@users.noreply.github.com>
2024-01-30 12:59:16 -0300

    --script

---
+++ script.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById("search-input");
+
+//escutando o evento
+document.addEventListener("input", function () {
+  const searchTearm = searchInput.value.toLowerCase();
+  if (searchTearm === "") {
+  }
+});
